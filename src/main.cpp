a0a0a37943f14e25d0b7a8c10d0cb34e3f2d4db3
Drew Smith <smithdrew555@yahoo.com>
2022-06-12 09:14:09 -0400

    added diffuese lighting

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,19 @@
 
 #include <iostream>
 
-[[nodiscard]] constexpr color ray_color(const ray &r, const hittable &world) {
+[[nodiscard]] constexpr color ray_color(const ray &r, const hittable &world,
+                                        int depth) {
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return color(0, 0, 0);
+
     constexpr auto one_half = 0.5;
     constexpr auto seven_tenths = 0.7;
-    if (auto rec = world.hit(r, 0, infinity)) {
-        return one_half * (rec->normal + color(1, 1, 1));
+    constexpr auto t_min = 0.001;
+    if (auto rec = world.hit(r, t_min, infinity)) {
+        point3 target = rec->p + rec->normal + random_in_unit_sphere();
+        return one_half *
+               ray_color(ray(rec->p, target - rec->p), world, depth - 1);
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = one_half * (unit_direction.y() + 1);
@@ -24,6 +32,7 @@ int main() {
     constexpr int image_width = 400;
     constexpr int image_height = static_cast<int>(image_width / aspect_ratio);
     constexpr int samples_per_pixel = 100;
+    constexpr int max_depth = 50;
 
     hittable_list world;
     world.add(make_unique<sphere>(point3(0, 0, -1), 0.5));
@@ -41,7 +50,7 @@ int main() {
                 auto u = (i + random_double()) / (image_width - 1);
                 auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
