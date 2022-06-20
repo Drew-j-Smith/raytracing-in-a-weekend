98f869e8b2965d6a4e555427784bac1640dd6160
Drew Smith <smithdrew555@yahoo.com>
2022-06-20 00:01:12 -0400

    added dear imgui

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,14 @@
 
 #include <spdlog/spdlog.h>
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <gl/glew.h>
+
+#include <GLFW/glfw3.h>
+
 void check_err(cl_int err) {
     if (err != CL_SUCCESS) {
         spdlog::dump_backtrace();
@@ -25,7 +33,7 @@ int main([[maybe_unused]] int argc, char **argv) {
     constexpr auto arr_size = 1000;
 
     spdlog::enable_backtrace(32);
-    spdlog::set_level(spdlog::level::debug);
+    spdlog::set_level(spdlog::level::info);
 
     cl_int err;
     auto queue = cl::CommandQueue(CL_QUEUE_PROFILING_ENABLE, &err);
@@ -85,6 +93,74 @@ int main([[maybe_unused]] int argc, char **argv) {
     }
 
     spdlog::info("success");
+
+    const char *glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    if (!glfwInit())
+        return 1;
+
+    // Create window with graphics context
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "test", NULL, NULL);
+    if (window == NULL)
+        return 1;
+    glfwMakeContextCurrent(window);
+
+    bool err2 = glewInit() != GLEW_OK;
+
+    if (err2) {
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        return 1;
+    }
+
+    int screen_width;
+    int screen_height;
+    glfwGetFramebufferSize(window, &screen_width, &screen_height);
+    glViewport(0, 0, screen_width, screen_height);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // feed inputs to dear imgui, start new frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // render your GUI
+        ImGui::Begin("Demo window");
+        ImGui::ShowDemoWindow();
+        ImGui::End();
+
+        // Render dear imgui into screen
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        int display_w;
+        int display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
