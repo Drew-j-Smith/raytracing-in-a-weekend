2883729ed096dd2d05fdb67ff4cbc8b23e108235
Drew Smith <smithdrew555@yahoo.com>
2022-06-22 00:10:59 -0400

    added better logging

--- src/main.cpp
+++ src/main.cpp
@@ -18,41 +18,35 @@
 
 #include <GLFW/glfw3.h>
 
-void check_err(cl_int err) {
-    if (err != CL_SUCCESS) {
-        spdlog::dump_backtrace();
-        spdlog::error("failed with error code: {}", err);
-        throw std::runtime_error("");
-    }
-}
-
 using arr_int_type = uint64_t;
 
 int main([[maybe_unused]] int argc, char **argv) {
 
     constexpr auto arr_size = 1000;
 
-    spdlog::enable_backtrace(32);
     spdlog::set_level(spdlog::level::info);
 
-    cl_int err;
-    auto queue = cl::CommandQueue(CL_QUEUE_PROFILING_ENABLE, &err);
-    check_err(err);
-    spdlog::debug("created CommandQueue");
+    auto ctx = cl::Context(
+        CL_DEVICE_TYPE_DEFAULT, nullptr,
+        [](const char *errinfo, [[maybe_unused]] const void *private_info,
+           [[maybe_unused]] size_t cb, [[maybe_unused]] void *user_data) {
+            spdlog::error("opencl error: {}", errinfo);
+        });
+    spdlog::info("created cl::Context");
+
+    auto queue = cl::CommandQueue(ctx, CL_QUEUE_PROFILING_ENABLE);
+    spdlog::info("created cl::CommandQueue");
 
     std::array<arr_int_type, arr_size> input;
     for (std::size_t i = 0; i < input.size(); ++i) {
         input[i] = static_cast<arr_int_type>(i);
     }
-    auto inputBuff =
-        cl::Buffer(queue, input.begin(), input.end(), true, false, &err);
-    check_err(err);
+    auto inputBuff = cl::Buffer(queue, input.begin(), input.end(), true, false);
 
     std::array<arr_int_type, arr_size> output;
     auto outputBuff =
-        cl::Buffer(queue, output.begin(), output.end(), false, false, &err);
-    check_err(err);
-    spdlog::debug("created Buffers");
+        cl::Buffer(queue, output.begin(), output.end(), false, false);
+    spdlog::info("created cl::Buffer(s)");
 
     std::filesystem::path exec_dir(argv[0]);
     std::string cl_loc = exec_dir.parent_path().append("main.cl").string();
@@ -60,33 +54,29 @@ int main([[maybe_unused]] int argc, char **argv) {
     std::string source((std::istreambuf_iterator<char>(file)),
                        std::istreambuf_iterator<char>());
 
-    cl::Program program(source, true, &err);
+    cl_int err;
+    cl::Program program(ctx, source, true, &err);
     if (err != CL_SUCCESS) {
-        cl::BuildLogType log = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(&err);
-        check_err(err);
+        cl::BuildLogType log = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>();
         for (const auto &pair : log) {
-            spdlog::error("{}", pair.second);
+            spdlog::error("Build error on Device {} \nBuild info:{}",
+                          pair.first.getInfo<CL_DEVICE_VENDOR>(), pair.second);
         }
-        return 0;
     }
-    spdlog::debug("created and compliled Program");
+    spdlog::info("created and compliled cl::Program");
 
-    cl::Kernel kernel_mult = cl::Kernel(program, "mult", &err);
-    check_err(err);
-    err = kernel_mult.setArg(0, inputBuff);
-    check_err(err);
-    err = kernel_mult.setArg(1, outputBuff);
-    check_err(err);
-    spdlog::debug("created Kernel");
+    cl::Kernel kernel_mult = cl::Kernel(program, "mult");
+    kernel_mult.setArg(0, inputBuff);
+    kernel_mult.setArg(1, outputBuff);
+    spdlog::info("created cl::Kernel");
 
-    err = queue.enqueueNDRangeKernel(kernel_mult, cl::NullRange,
-                                     cl::NDRange(arr_size), cl::NullRange);
-    check_err(err);
+    queue.enqueueNDRangeKernel(kernel_mult, cl::NullRange,
+                               cl::NDRange(arr_size), cl::NullRange);
+    queue.finish();
 
-    err = queue.enqueueReadBuffer(outputBuff, CL_TRUE, 0,
-                                  output.size() * sizeof(arr_int_type),
-                                  output.data());
-    check_err(err);
+    queue.enqueueReadBuffer(outputBuff, CL_TRUE, 0,
+                            output.size() * sizeof(arr_int_type),
+                            output.data());
 
     for (auto i : output) {
         spdlog::debug("{}", i);
@@ -98,19 +88,27 @@ int main([[maybe_unused]] int argc, char **argv) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    if (!glfwInit())
+    if (!glfwInit()) {
+        spdlog::error("glfwInit failed");
         return 1;
+    }
+
+    glfwSetErrorCallback([](int errCode, const char *msg) {
+        spdlog::error("glfw errored with code {}: {}", errCode, msg);
+    });
 
     // Create window with graphics context
     GLFWwindow *window = glfwCreateWindow(1280, 720, "test", NULL, NULL);
-    if (window == NULL)
+    if (window == NULL) {
+        spdlog::error("glfwCreateWindow failed");
         return 1;
+    }
     glfwMakeContextCurrent(window);
 
     bool err2 = glewInit() != GLEW_OK;
 
     if (err2) {
-        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        spdlog::error("Failed to initialize OpenGL loader!");
         return 1;
     }
 
@@ -138,41 +136,43 @@ int main([[maybe_unused]] int argc, char **argv) {
         }
     }
 
-    auto errTest = []() {
-        GLenum err;
-        if ((err = glGetError()) != GL_NO_ERROR) {
-            throw new std::exception("");
-        }
-    };
-
     glDebugMessageCallback(
         [](GLenum source, GLenum type, GLuint id, GLenum severity,
-           GLsizei length, const GLchar *message,
-           const void *userParam) { spdlog::info("{}", message); },
+           GLsizei length, const GLchar *message, const void *userParam) {
+            auto formated = fmt::format("opengl debug: source: {} type: {} id: "
+                                        "{} severity: {}\n message: {}",
+                                        source, type, id, severity, message);
+            switch (severity) {
+            case GL_DEBUG_SEVERITY_NOTIFICATION:
+                spdlog::info(formated);
+                break;
+            case GL_DEBUG_SEVERITY_LOW:
+                spdlog::warn(formated);
+                break;
+            case GL_DEBUG_SEVERITY_MEDIUM:
+                spdlog::warn(formated);
+                break;
+            case GL_DEBUG_SEVERITY_HIGH:
+                spdlog::error(formated);
+                break;
+            }
+        },
         nullptr);
     glEnable(GL_DEBUG_OUTPUT);
 
     GLuint texture;
     glGenTextures(1, &texture);
-    errTest();
     glBindTexture(GL_TEXTURE_2D, texture);
-    errTest();
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, 512, 512, 0, GL_RGB,
                  GL_UNSIGNED_BYTE, nullptr);
-    errTest();
     glBindTexture(GL_TEXTURE_2D, 0);
-    errTest();
 
     GLuint readFboId = 0;
     glGenFramebuffers(1, &readFboId);
-    errTest();
     glBindFramebuffer(GL_READ_FRAMEBUFFER, readFboId);
-    errTest();
     glFramebufferTexture2D(GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
                            GL_TEXTURE_2D, texture, 0);
-    errTest();
     glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
-    errTest();
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
